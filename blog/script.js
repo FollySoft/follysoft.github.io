a01c5ac2afbc8e10933f5fff178fe9bf480169c8
jwest <jwest@psl.nmsu.edu>
2023-05-17 12:49:15 -0600

    Fixed the posts array I think?

--- blog/script.js
+++ blog/script.js
@@ -34,8 +34,9 @@ let postsArray = [
 //[ "posts/2020-11-10-Special-Characters-Example.html", encodeURI( 'Spéci@l "Character\'s" Examp|e' ) ]
 //[ "posts/2020-11-10-My-Third-Post-Example.html" ],
 //[ "posts/2020-11-10-My-Second-Post-Example.html" ],
-[ "posts/2023-05-02-OWS-Jam.html",
-  "posts/2023-05-17-Pespectives-Game-Devlog-1.html" ] ];
+  [ "posts/2023-05-02-OWS-Jam.html"],
+  [ "posts/2023-05-17-Pespectives-Game-Devlog-1.html" ] 
+];
 
 //-----------------------------
 
